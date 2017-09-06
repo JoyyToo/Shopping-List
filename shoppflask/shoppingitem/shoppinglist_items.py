70a189d2b7d3103f09681009b5904af4f878275f
kalitrial <kalitrial@gmail.com>
2017-09-06 21:43:28 +0300

    [chore]Add updated shopping list items module

--- shoppflask/shoppingitem/shoppinglist_items.py
+++ shoppflask/shoppingitem/shoppinglist_items.py
@@ -1,3 +1,6 @@
+"""ShoppingList items class"""
+
+
 class ListItems(object):
     """
     Users can add, update, view or delete items in a shopping list
@@ -5,14 +8,16 @@ class ListItems(object):
 
     items = {}
     categories = [
-        'Groceries', 'Bathroom Supplies', 'Household Supplies', 'Office Supplies', 'Kitchen Supplies']
+        'Groceries', 'Bathroom Supplies', 'Household Supplies',
+        'Office Supplies', 'Kitchen Supplies']
 
     def __init__(self):
+        """Initializes the class"""
         self.name = None
         self.category = None
         self.amount = None
 
-    def add_item(self, name, amount, category):
+    def add_item(self, name, amount, category, bucket):
         """Add items to a list"""
 
         if name != '' and amount != '' and category != '':
@@ -24,10 +29,9 @@ class ListItems(object):
             self.items[name] = {
                 "name": name,
                 "amount": amount,
-                "cat": category
+                "cat": category,
+                "bucket": bucket
             }
-
-            print(self.items)
             return {
                 "type": "success",
                 "msg": "Item successfully added"
@@ -39,47 +43,55 @@ class ListItems(object):
 
     def update_item(self, name, amount, category):
         """Update items in a list"""
-        self.name = name
-        self.amount = amount
-        self.category = category
-        if self.name != '' and self.amount != '' and self.category != '':
-            if self.name in self.items.keys():
-                return {
-                    "type": "success",
-                    "data": self.updatedata()
-                }
+        if name and amount and category:
+            if self.name in self.items:
+                # returns original list under specificed list name
+                original_list = self.items[self.name]
+                # remove the original list name
+                original_list.remove(self.name)
+                # updated list by substituting names
+                original_list.append(self.amount)
+                temporary_dict = {self.name: original_list}
+                del self.items[self.name]
+                self.items.update(temporary_dict)
+                return self.items
             else:
                 return {
-                    "type": "error",
-                    "msg": "Item Unavailable"
+                    'type': 'error',
+                    'msg': 'Item unavailable'
                 }
         else:
             return {
                 "type": "error",
-                'msg': 'Fill in all fields'
+                "msg": 'Fill in all fields'
             }
 
     def view_item(self, name):
         """User can view items in a list"""
-        sl = []
-        if name not in self.items.keys():
-            return {
-                "type": "error",
-                "msg": "Item Unavailable"
-            }
-        self.items[name] = sl.append(name)
+        data = []
+        if self.items:
+            for item in self.items:
+                if self.items[item]['bucket'] == name:
+                    data.append(self.items[item])
+            if data:
+                return {
+                    "type": "success",
+                    "data": data
+                }
         return {
-            "type": "success",
-            "data": self.items.pop(name)
+            "type": "error",
+            "msg": "Items not available at the moment."
         }
 
     def delete_item(self, name):
         """Delete items in a list"""
         self.name = name
         if self.name in self.items.keys():
+            self.items.pop(self.name)
+
             return {
                 "msg": "Item deleted successfully",
-                "data": self.items.pop(self.name)
+                "data": self.items
             }
 
         else:
@@ -114,7 +126,7 @@ class ListItems(object):
 
     def updatedata(self):
         """Updates items entered in a list"""
-        if self.category in self.categories:
+        if self.name in self.items.keys():
             self.items[self.name] = {
                 'name': self.name,
                 'category': self.category,
@@ -129,11 +141,3 @@ class ListItems(object):
                 'type': 'error',
                 'msg': 'Category Unrecognized'
             }
-
-    def all_shoppinglistitems(self):
-        return self.items
-
-    def single_shoppinglistitem(self, id):
-        return self.items[id]
-
-
